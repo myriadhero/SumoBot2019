a0af514d6e5cda7366f4284e17dd3432ff7fbe97
MyriadHero <kddreauw@gmail.com>
2018-12-28 17:38:27 +0800

    still missing some logic

--- src/main.cpp
+++ src/main.cpp
@@ -7,22 +7,56 @@ this will probably only include high level functions without specific implementa
 
 */
 
+// includes
+
+
+
+// I2C and other initiations
+
+indicators indics;
+motorControl Mcontrol;
+
+// constants
+
+#define ARENADIAM 1500
+#define FRONT 1
+#define BACK 0
+#define ADVSEEK 1
+
+
+
+// global variables
+// toDo: volatile everything
+
+
+volatile bool gameOn = 0;
+
+volatile bool borderNotDetected = 1; // toDo: think about border detection 
+volatile bool borderFR = 0;         // behaviours; it's important
+volatile bool borderFL = 0;         // since it's the losing criteria
+volatile bool borderF = 0;
+volatile bool borderBR = 0;
+volatile bool borderBL = 0; 
+volatile bool borderB = 0;
+
 // prototype functions
 
-
 void locate(bool frontBack);
-void hide(){
-
-}
-
-void seek();
+void hide();
+bool seek(bool advanced = 0); // returns 1 if found
+bool isP2coming(); // returns 1 for yes, quick check whether p2 is approaching fast
 void evade();
 void attack();
+void celebrate();
+// void borderISR();
+// void encodersISR();
 
 class motorControl{
     public:
 
     int initParams = 0;
+    unsigned int encRight = 0;
+    unsigned int encLeft = 0;
 
 
     void setMotor(bool leftRight, int motorPWM);
@@ -35,10 +69,84 @@ class motorControl{
 
 };
 
+class indicators{
+    public:
+
+    void initInd(){
+        // when the robot is ready to go
+    }
+
+    void gameStart(){
+
+    }
+
+    void aboutTown(){
+
+    }
+
+    void seek(){
+
+    }
+
+    void attack(){
+
+    }
+
+    void celebrate(){
+
+    }
+
+
+};
+
 
 int main() {
+
+    // initialise
     
 
+
+
+    indics.initInd(); //ready to go
+
+
+    while(!gameOn){
+        // wait for start command
+    }
+    
+    while(gameOn){
+
+        locate(FRONT); // drive forward and activate both sensors
+        hide();
+        if(seek()){
+            if(isP2coming()){
+                // 75% chance to initiate evade, 25% chance to initiate attack
+            }
+            else{
+                // 10% chance to initiate evade, 90% chance to initiate attack
+            }
+        }
+        
+        else{
+            if(seek(ADVSEEK)){
+                if(isP2coming()){
+                    // 75% chance to initiate evade, 25% chance to initiate attack
+                }
+                else{
+                    // 10% chance to initiate evade, 90% chance to initiate attack
+                }
+            }
+            else{
+                celebrate(); // celebrate and set loop values to exit
+                break;
+            }
+        }
+
+
+        
+    } // close tag for while(gameOn)
+    
+    //end of the game routine
 
 
 }
