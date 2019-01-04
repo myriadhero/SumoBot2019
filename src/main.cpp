7d27bdd2d31bf87a22d3c3f7d9e556d4eead9cee
MyriadHero <kddreauw@gmail.com>
2019-01-04 17:28:03 +0800

    some more logic

--- src/main.cpp
+++ src/main.cpp
@@ -30,6 +30,8 @@ motorControl Mcontrol;
 
 
 volatile bool gameOn = 0;
+volatile bool gameMode = 0; // simple mode for phase1 and phase 2
+volatile bool phase = 0;
 
 volatile bool borderNotDetected = 1; // toDo: think about border detection 
 volatile bool borderFR = 0;         // behaviours; it's important
@@ -41,11 +43,20 @@ volatile bool borderB = 0;
 
 // prototype functions
 
-void locate(bool frontBack);
-void hide();
-bool seek(bool advanced = 0); // returns 1 if found
+void locate(bool frontBack); // find an arena edge from front or back
+void hide(int hideKind);    // maneuvers that involve evasion
+
+int seek(bool leftRight, bool advanced = 0){ // returns direction if found, otherwise 0
+    int angle1 = 0;
+    int angle2 = 0;
+    //start sweeping the servo in the direction of turnDegrees
+    // +1 degree, collect radar info
+    // if radar info == P2, collect first angle
+    // when radar info != P2, collect second angle
+    // sweep back to confirm, correct angles if needed
+    return ((angle1+angle2)/2);
+}
 bool isP2coming(); // returns 1 for yes, quick check whether p2 is approaching fast
-void evade();
 void attack();
 void celebrate();
 // void borderISR();
@@ -117,7 +128,7 @@ int main() {
     while(gameOn){
 
         locate(FRONT); // drive forward and activate both sensors
-        hide();
+        hide(1);
         if(seek()){
             if(isP2coming()){
                 // 75% chance to initiate evade, 25% chance to initiate attack
